94569af739f62b949e3ee92280068d42e9879a9f
Phúc Nguyễn <mrvntech@gmail.com>
2023-08-17 23:54:17 +0700

    refactor homework 7

--- src/homework7/TeacherController.java
+++ src/homework7/TeacherController.java
@@ -1,23 +1,20 @@
 package homework7;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class TeacherController {
-    private HashSet<Teacher> teachers = new HashSet<>();
+    private HashMap<Integer, Teacher> teachers = new HashMap<>();
 
     public void addTeacher(Teacher teacher) {
-        teachers.add(teacher);
+        teachers.put(teacher.getId(), teacher);
     }
 
     public void removeTeacher(int id) {
-        teachers.stream().filter(teacher -> {
-            return teacher.getId() == id;
-        }).forEach(teacher -> {
-            teachers.remove(teacher);
-        });
-
+        teachers.remove(id);
     }
+
 }
